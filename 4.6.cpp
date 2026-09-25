e0d7efcddfbcfbf39ca6f308e3cee3b57cb02e40
Акобян Роберт Наполеонович <106011215@ACADEMY.TOP>
2026-09-25 14:33:43 +0300

    finished 4.6"

--- 4.6.cpp
+++ 4.6.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
+#include <list>
+#include <algorithm>
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 	int input_count_number;
 	int number;
+	std::list<int> list_number;
 	int sum = 0;
 	std::cout << "Введите количество чисел: ";
 	std::cin >> input_count_number;
 	for (int i = 0; i < input_count_number; i++) {
 		std::cin >> number;
-		list_number[i] = number;
+		list_number.push_back(number);
 	};
-	std::cout << maxNumber;
-	std::cout << minNumber;
-	std::cout << sum / 2;
+	auto maxNumber = std::max_element(list_number.begin(), list_number.end());
+	auto minNumber = std::min_element(list_number.begin(), list_number.end());
+	sum = *maxNumber - *minNumber;
+	std::cout << "\nМаксимальное: " << *maxNumber;
+	std::cout << "\nМинимальное: " << *minNumber;
+	if (sum < 0) {
+		std::cout << "\nРазница между ними: " << 0;
+	}
+	else {
+		std::cout << "\nРазница между ними: " << sum;
+	}
 }
